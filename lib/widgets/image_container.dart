34463764c5256600adf4af236d5a0d4e8855eb70
Omotayo <omotayo4david@gmail.com>
2025-02-23 17:51:44 +0100

    image container animation completed

--- lib/widgets/image_container.dart
+++ lib/widgets/image_container.dart
@@ -14,22 +14,28 @@ class ImageContainer extends StatefulWidget {
 }
 
 class _ImageContainerState extends State<ImageContainer> with TickerProviderStateMixin {
-  double _buttonSliderPosition = 0.0;
-
   late AnimationController _animationController;
-  late Animation _animation;
+  late Animation<double> _animation;
+
+  var _width = 44.0;
 
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _animation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+
     Future.delayed(Duration(seconds: 1), () {
       setState(() {
-        _buttonSliderPosition = widget.width - 76;
+        _width = widget.width;
       });
     });
+  }
 
-    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 2));
-    _animation = Tween<double>(begin: -40, end: 16).animate(_animationController);
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -51,54 +57,67 @@ class _ImageContainerState extends State<ImageContainer> with TickerProviderStat
               builder: (context, constraint) {
                 return Stack(
                   children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(40),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                        child: Container(
-                          height: 44,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(44),
-                            color: Colors.white.withOpacity(0.6),
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.white.withOpacity(0.6),
-                                Colors.white.withOpacity(0.4),
+                    AnimatedContainer(
+                      width: _width,
+                      duration: Duration(seconds: 1),
+                      onEnd: () {
+                        _animationController
+                          ..reset()
+                          ..forward();
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(44),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                          child: Container(
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(44),
+                              color: Colors.white.withOpacity(0.6),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.white.withOpacity(0.6),
+                                  Colors.white.withOpacity(0.4),
+                                ],
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                            child: Stack(
+                              children: [
+                                FadeTransition(
+                                  opacity: _animation,
+                                  child: Center(
+                                    child: Text(
+                                      "Gladkova St., 25",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Container(
+                                    width: 36,
+                                    height: 36,
+                                    margin: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      shape: BoxShape.circle,
+                                      boxShadow: [
+                                        BoxShadow(color: Colors.black26, blurRadius: 5),
+                                      ],
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Icon(CupertinoIcons.chevron_right, color: Colors.grey, size: 16),
+                                  ),
+                                )
                               ],
                             ),
                           ),
-                          alignment: Alignment.center,
-                          child: AnimatedBuilder(
-                              animation: _animation,
-                              builder: (context, child) {
-                                return Text(
-                                  "Gladkova St., 25",
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                                );
-                              }),
                         ),
-                      ),
-                    ),
-                    AnimatedPositioned(
-                      duration: Duration(milliseconds: 400),
-                      left: _buttonSliderPosition,
-                      child: Container(
-                        width: 36,
-                        height: 36,
-                        margin: EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(color: Colors.black26, blurRadius: 5),
-                          ],
-                        ),
-                        alignment: Alignment.center,
-                        child: Icon(CupertinoIcons.chevron_right, color: Colors.grey, size: 16),
                       ),
                     ),
                   ],
